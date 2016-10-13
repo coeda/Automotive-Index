62df9f7d7c8a2513e656236ac53eede406b4501d
Casey Oeda <coeda@live.com>
2016-10-12 19:51:16 -1000

    completed

---
+++ indexing.sql
@@ -0,0 +1,14 @@
+-- 54.08ms
+-- 56.34ms
+-- 51.876ms
+-- 137.293ms
+-- 73.009ms
+
+CREATE INDEX maketitles ON car_models (make_title);
+CREATE INDEX modeltitles ON car_models (model_title, make_code);
+
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+SELECT * FROM car_models WHERE year = 2010;
--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,6 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+CREATE INDEX maketitles ON car_models (make_title);
+CREATE INDEX modeltitles ON car_models (model_title, make_code);
